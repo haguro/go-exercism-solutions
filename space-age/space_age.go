1c58cda185b4693e91ea7e2f1858023ad21a3a38
haguro <1076+haguro@users.noreply.github.com>
2019-03-05 23:12:21 +1100

    Go/Space Age

---
+++ space-age/space_age.go
@@ -0,0 +1,21 @@
+package space
+
+type Planet string
+
+const earth_op = 31557600
+
+// Orbital periods in seconds relative to Earth's
+var opr = map[Planet]float64{
+	"Earth":   1.0,
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
+}
+
+func Age(seconds float64, planet Planet) float64 {
+	return seconds / (earth_op * opr[planet])
+}

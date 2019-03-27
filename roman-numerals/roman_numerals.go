f4438e53b83b270b6090bf43f69cf5c98e797a3e
haguro <1076+haguro@users.noreply.github.com>
2019-03-27 21:53:41 +1100

    Roman Numerals

---
+++ roman-numerals/roman_numerals.go
@@ -0,0 +1,37 @@
+// Package romannumerals provides functionality to convert arabic numbers to roman numerals.
+package romannumerals
+
+import (
+	"errors"
+)
+
+var patternMap = map[int]string{
+	1000: "M",
+	900:  "CM",
+	500:  "D",
+	400:  "CD",
+	100:  "C",
+	90:   "XC",
+	50:   "L",
+	40:   "XL",
+	10:   "X",
+	9:    "IX",
+	5:    "V",
+	4:    "IV",
+	1:    "I",
+}
+
+// ToRomanNumeral convert an integer between 1 and 3000 to a roman numeral equivalent.
+func ToRomanNumeral(arabic int) (string, error) {
+	if arabic <= 0 || arabic > 3000 {
+		return "", errors.New("arabic number out of bound")
+	}
+	var roman string
+	for _, n := range []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1} {
+		for arabic >= n {
+			roman += patternMap[n]
+			arabic -= n
+		}
+	}
+	return roman, nil
+}

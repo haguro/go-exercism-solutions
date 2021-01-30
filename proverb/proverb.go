c745759f110f533f2b6cf984c2b18771e7e6a8ac
haguro <1076+haguro@users.noreply.github.com>
2021-01-30 20:49:47 +1100

    Proverb
    Fix print bug, update to latest version.

--- proverb/proverb.go
+++ proverb/proverb.go
@@ -7,7 +7,7 @@ import "fmt"
 func Proverb(rhyme []string) []string {
 	var proverb []string
 	for i := 0; i < len(rhyme)-1; i++ {
-		proverb = append(proverb, fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i], rhyme[i-1]))
+		proverb = append(proverb, fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i], rhyme[i+1]))
 	}
 	if len(rhyme) > 0 {
 		proverb = append(proverb, fmt.Sprintf("And all for the want of a %s.", rhyme[0]))

98635bf86160a8d1654e6058d45bb26fd0923ef6
haguro <1076+haguro@users.noreply.github.com>
2021-01-30 22:05:37 +1100

    Roman Numerals
    Use package var for Roman symbol values.

--- roman-numerals/roman_numerals.go
+++ roman-numerals/roman_numerals.go
@@ -1,11 +1,13 @@
-// Package romannumerals provides functionality to convert arabic numbers to roman numerals.
+// Package romannumerals provides functionality to convert Arabic numbers to
+// Roman numerals.
 package romannumerals
 
 import (
 	"errors"
 )
 
-var patternMap = map[int]string{
+var symbolValues = []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+var symbolMap = map[int]string{
 	1000: "M",
 	900:  "CM",
 	500:  "D",
@@ -27,10 +29,10 @@ func ToRomanNumeral(arabic int) (string, error) {
 		return "", errors.New("arabic number out of bound")
 	}
 	var roman string
-	for _, n := range []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1} {
-		for arabic >= n {
-			roman += patternMap[n]
-			arabic -= n
+	for _, v := range symbolValues {
+		for arabic >= v {
+			roman += symbolMap[v]
+			arabic -= v
 		}
 	}
 	return roman, nil

8e297f766630b583ad59073c95bf60344e5e6017
haguro <1076+haguro@users.noreply.github.com>
2021-02-11 18:28:48 +1100

    Use channels to solve the exercise

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,10 +1,6 @@
 // Package letter provides functions to calculate the Frequency of letters in strings.
 package letter
 
-import (
-	"sync"
-)
-
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
 
@@ -19,55 +15,20 @@ func Frequency(s string) FreqMap {
 }
 
 // ConcurrentFrequency concurrently calls `Frequency` on multiple strings and
-// Combine the results.
+// combines the results.
 func ConcurrentFrequency(xs []string) FreqMap {
-	//return concurrentFrequencyWithSyncMaps(xs)
-	return concurrentFrequencyWithMutex(xs)
-}
-
-func concurrentFrequencyWithSyncMaps(xs []string) FreqMap {
-	var (
-		sm sync.Map
-		wg sync.WaitGroup
-	)
 	fm := FreqMap{}
+	c := make(chan FreqMap, len(xs))
 	for _, s := range xs {
-		wg.Add(1)
-		go func(s string) {
-			for r, f := range Frequency(s) {
-				storedF, loaded := sm.LoadOrStore(r, f)
-				if loaded {
-					sm.Store(r, f+storedF.(int))
-				}
-			}
-			wg.Done()
-		}(s)
-	}
-	wg.Wait()
-	sm.Range(func(key interface{}, value interface{}) bool {
-		fm[key.(rune)] = value.(int)
-		return true
-	})
-	return fm
-}
+		go func(s string, c chan FreqMap) {
+			c <- Frequency(s)
+		}(s, c)
 
-func concurrentFrequencyWithMutex(xs []string) FreqMap {
-	var (
-		mx sync.Mutex
-		wg sync.WaitGroup
-	)
-	fm := FreqMap{}
-	for _, s := range xs {
-		wg.Add(1)
-		go func(s string) {
-			for r, f := range Frequency(s) {
-				mx.Lock()
-				fm[r] += f
-				mx.Unlock()
-			}
-			wg.Done()
-		}(s)
 	}
-	wg.Wait()
+	for range xs {
+		for r, f := range <-c {
+			fm[r] += f
+		}
+	}
 	return fm
 }

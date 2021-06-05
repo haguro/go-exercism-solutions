80bafaf866c702c4486c4261f436b6c0afa01d76
haguro <1076+haguro@users.noreply.github.com>
2021-06-06 00:27:57 +1000

    Twelve Days
    Move gift count to an anonymous struct slice.

--- twelve-days/twelve_days.go
+++ twelve-days/twelve_days.go
@@ -7,34 +7,36 @@ import (
 
 const template = "On the %s day of Christmas my true love gave to me: %s."
 
-var ordinals = []string{
-	"first",
-	"second",
-	"third",
-	"fourth",
-	"fifth",
-	"sixth",
-	"seventh",
-	"eighth",
-	"ninth",
-	"tenth",
-	"eleventh",
-	"twelfth",
+var numbers = []struct {
+	cardinal, ordinal string
+}{
+	{"a", "first"},
+	{"two", "second"},
+	{"three", "third"},
+	{"four", "fourth"},
+	{"five", "fifth"},
+	{"six", "sixth"},
+	{"seven", "seventh"},
+	{"eight", "eighth"},
+	{"nine", "ninth"},
+	{"ten", "tenth"},
+	{"eleven", "eleventh"},
+	{"twelve", "twelfth"},
 }
 
 var gifts = []string{
-	"a Partridge in a Pear Tree",
-	"two Turtle Doves",
-	"three French Hens",
-	"four Calling Birds",
-	"five Gold Rings",
-	"six Geese-a-Laying",
-	"seven Swans-a-Swimming",
-	"eight Maids-a-Milking",
-	"nine Ladies Dancing",
-	"ten Lords-a-Leaping",
-	"eleven Pipers Piping",
-	"twelve Drummers Drumming",
+	"Partridge in a Pear Tree",
+	"Turtle Doves",
+	"French Hens",
+	"Calling Birds",
+	"Gold Rings",
+	"Geese-a-Laying",
+	"Swans-a-Swimming",
+	"Maids-a-Milking",
+	"Ladies Dancing",
+	"Lords-a-Leaping",
+	"Pipers Piping",
+	"Drummers Drumming",
 }
 
 //Song combines all twelves verses into a single string
@@ -53,15 +55,15 @@ func Verse(n int) string {
 	}
 	var b strings.Builder
 
-	for i := 1; i < n; i++ {
-		b.WriteString(gifts[n-i])
-		b.WriteString(", ")
+	for i := n; i > 0; i-- {
+		b.WriteString(numbers[i-1].cardinal + " " + gifts[i-1])
+		if i > 1 {
+			b.WriteString(", ")
+		}
+		if n > 1 && i == 2 {
+			b.WriteString("and ")
+		}
 	}
 
-	if n > 1 {
-		b.WriteString("and ")
-	}
-	b.WriteString(gifts[0])
-
-	return fmt.Sprintf(template, ordinals[n-1], b.String())
+	return fmt.Sprintf(template, numbers[n-1].ordinal, b.String())
 }
